345ad70b3587f4197636e872860f3fc36d6987bd
tengxunlaozu <44842903+tengxunlaozu@users.noreply.github.com>
2026-08-31 10:12:01 +0800

    Add passwall and passwall_packages updates
    Added additional sources for passwall and passwall_packages updates.

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -52,6 +52,8 @@ UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
 UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
+UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
+UPDATE_PACKAGE "passwall_packages" "xiaorouji/openwrt-passwall-packages" "main" "pkg"
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
